68dca6e6cea297fdad8074248402273aa2403044
Bayr_kf60 <Bayr_kf60@STHQ0128-04>
2022-08-30 11:22:49 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button btn)
+            {
+                Random r = new Random();
+                Brush brush = new SolidColorBrush(Color.FromRgb((byte)r.Next(1, 255),
+                                  (byte)r.Next(1, 255), (byte)r.Next(1, 233)));
+                btn.Background = brush;
+            }
+            
+        }
+
+        private void btn1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+        private void btn1_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is Button btn)
+            {
+              this.Title = btn.Content.ToString();
+            }
+        }
+    }
+}
